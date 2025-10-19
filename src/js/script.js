1c0b98f59c97c29d1fc6b118afdfb5e3e6678c41
Darshannaik484 <darshannnaik484@gmail.com>
2025-10-19 13:13:27 +0530

    Added javascript files

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,15 @@
 <script>
 function redirectToDonate() {
-    alert("You are redirecting to Donate Blood page...");
-    setTimeout(function() {
-        window.location.href = "DonatorHome.html";
-    }, 2000); // 2-second delay
+  alert("You are redirecting to Donate Blood page...");
+  setTimeout(function() {
+    window.location.href = "../src/html/DonatorHome.html";
+  }, 2000); // 2-second delay
+}
+
+function redirectToRequest() {
+  alert("You are redirecting to Request Blood page...");
+  setTimeout(function() {
+    window.location.href = "../src/html/ReceiverHome.html";
+  }, 2000); // 2-second delay
 }
 </script>
